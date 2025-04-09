7d071f93c9a33a516d816ec2a401b74627c4d914
luizazbl <luizaizabeldeandrade@gmail.com>
2025-04-09 20:43:40 -0300

    Create atividade10.java

---
+++ atividade10.java
@@ -0,0 +1,11 @@
+public class inteiro {
+public static void main (Stringll args) {
+int num2;
+System. out. println(x:"segundo numero: ");
+num2 =scanner.nextInt();
+int menor = Math.min (numi, num2);
+int maior = Math.max (numi, num2) ;
+for (int i = menor + 1; i ‹ maior; i++) {
+System. out. printin(i);
+System. out println(x: "numeros entre numi e num2: ");
+scanner. close();
