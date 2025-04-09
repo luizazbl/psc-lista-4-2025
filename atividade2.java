9964aa75f8a45c97c1046c2967135b152460db7d
luizazbl <luizaizabeldeandrade@gmail.com>
2025-04-09 19:11:32 -0300

    Create atividade2.java

---
+++ atividade2.java
@@ -0,0 +1,11 @@
+while (true) {
+System. out print(s:"Digite o nome de usuário: ");
+usuario = scanner.nextLine();
+System. out-print(s:"Digite a senha: ");
+senha = scanner.nextLine();
+if (!senha. equals(usuario)) {
+}
+else {
+System. out.println(x:"Senha incorreta. Tente novamente.");
+}
+scanner.close();
