d7a8d9f558f4bfce385f72ff8f75f16628db1314
luizazbl <luizaizabeldeandrade@gmail.com>
2025-04-09 20:42:57 -0300

    Create atividade9.java

---
+++ atividade9.java
@@ -0,0 +1,8 @@
+package lu;
+public class impar 1
+Run | Debug
+public static void main (Stringl] args) {
+
+for (int i = 1; i <= 50; i += 2) {
+System.out.println(x:"estes são os numero impares entre 1 e 50: ");
+System. out. println (i);
