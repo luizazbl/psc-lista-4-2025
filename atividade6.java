8d2b643690df3ffdbb8b6312a3f37342b6ebe456
luizazbl <luizaizabeldeandrade@gmail.com>
2025-04-09 19:18:11 -0300

    Create atividade6.java

---
+++ atividade6.java
@@ -0,0 +1,11 @@
+public static void main (Stringl] args) {
+Scanner scanner = new Scanner (System. in);
+PROBLEMAS (1)
+int populacaoA = 80000, populacaoB = 200000, anos = 0;
+double taxaCrescimentoA = 0.03, taxaCrescimentoB = 0.015;
+while (populacaoA <= populacaoB) { populacaoA += populacaoA * taxaCrescimentoA;
+populacaoB += populacaoB * taxaCrescimentoB;
+anos++;
+System.out println("Número de anos necessários: " + anos);
+scanner. close();
+System.out.print(i + " ");
