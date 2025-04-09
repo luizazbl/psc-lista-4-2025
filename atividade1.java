747cece35b407ba7484273c6c8e7bbf0c03893a4
luizazbl <luizaizabeldeandrade@gmail.com>
2025-04-09 19:11:00 -0300

    Create atividade1.java

---
+++ atividade1.java
@@ -0,0 +1,12 @@
+public static void main(Stringll args) {
+Scanner scanner = new Scanner (System. in) ;
+int nota;
+while (true) {
+System.out.print(s:"Digite uma nota entre 0 e 10: ");
+nota = scanner.nextInt();
+if (nota › 1 && nota <=10){
+}
+else {
+System.out.println(x:"Nota inválida! Tente novamente.");
+} System.out.println("Nota registrada: " + nota);
+scanner.close();
